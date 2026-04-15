1caa7456de193889c4ebae9f4ed47c59f2dd6d43
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-04-16 00:04:38 +0300

    restructure

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
-import * as schema from "./db/schema";
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
@@ -11,4 +11,4 @@ const sql = neon(process.env.DATABASE_URL);
 
 export const db = drizzle(sql, { schema });
 
-export * from "./db/schema";
+export * from "./schema";
