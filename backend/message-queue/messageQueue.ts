d191572a97963f8e8527d8cac6dfab435a746baf
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-04-18 12:01:55 +0300

    init messageQueue

---
+++ backend/message-queue/messageQueue.ts
@@ -0,0 +1,49 @@
+import amqplib from "amqplib";
+import type { Channel, ChannelModel, ConsumeMessage } from "amqplib";
+
+class MQ {
+  private conn!: ChannelModel;
+  private channel!: Channel;
+  private connecting?: Promise<void>;
+
+  async connect(url: string) {
+    if (this.conn) return;
+
+    if (this.connecting) {
+      await this.connecting;
+      return;
+    }
+
+    this.connecting = (async () => {
+      this.conn = await amqplib.connect(url);
+      this.channel = await this.conn.createChannel();
+    })();
+
+    await this.connecting;
+    this.connecting = undefined;
+  }
+  async sendEvent(queue: string, data: unknown) {
+    await this.channel.assertQueue(queue);
+    this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
+  }
+
+  async listen(queue: string, handler: (data: any) => void) {
+    await this.channel.assertQueue(queue);
+
+    this.channel.consume(queue, (msg: ConsumeMessage | null) => {
+      if (!msg) return;
+
+      try {
+        const data = JSON.parse(msg.content.toString());
+        handler(data);
+        this.channel.ack(msg);
+      } catch (err) {
+        console.error("Failed to process message", err);
+      }
+    });
+  }
+}
+
+const mq = new MQ();
+await mq.connect("amqp://localhost");
+export { mq };
