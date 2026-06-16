6cd8f3faad054458184a08cab3b62b0b4fe84a80
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-06-17 01:49:55 +0300

    replace OpenAI client with general OpenRouter client

--- backend/shared/ai/summarize.ts
+++ backend/shared/ai/summarize.ts
@@ -1,10 +1,9 @@
-import { openai } from "./ai_client";
+import { promptAI } from "./ai_client";
 
 export async function summarize(text: string) {
-  const res = await openai.responses.create({
-    model: "openai/gpt-4o-mini", // or any model on OpenRouter
-    input: `Summarize the following text:\n\n${text}`,
-  });
-
-  return res.output_text;
+  const res = await promptAI(
+    "openai/gpt-4o-mini", // or any model on OpenRouter
+    `Summarize the following text:\n\n${text}`,
+  );
+  return res;
 }
