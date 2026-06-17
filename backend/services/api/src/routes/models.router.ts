25dd4e43bc4731cd2f0a95dee15e7603570877d4
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-06-17 14:36:41 +0300

    models router and controller

---
+++ backend/services/api/src/routes/models.router.ts
@@ -0,0 +1,10 @@
+import { Hono } from "hono";
+import * as modelsController from "../controllers/models.controller";
+import { modelRateLimiter } from "../middleware/rateLimit.middleware";
+import { requireAuth } from "../middleware/auth.middleware";
+
+export const modelsRouter = new Hono();
+
+modelsRouter.use("*", requireAuth, modelRateLimiter);
+
+modelsRouter.get("/", modelsController.handleGetModels);
