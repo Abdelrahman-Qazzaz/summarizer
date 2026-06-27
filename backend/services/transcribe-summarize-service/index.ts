e6b824492841ab3aa2367796f5c618ef04946701
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-06-28 00:38:34 +0300

    implement same pattern for app creation in transcribe-summarize-service as api

--- backend/services/transcribe-summarize-service/index.ts
+++ backend/services/transcribe-summarize-service/index.ts
@@ -2,17 +2,12 @@ import { getWorkerEnv } from "../../shared/env";
 
 const env = getWorkerEnv();
 
-import { Hono } from "hono";
-import { cors } from "hono/cors";
 import { startMQ } from "../../shared/message-queue/messageQueue";
 import { attachListeners } from "./workers";
 import { serve } from "@hono/node-server";
+import { createApp } from "./app";
 
-const app = new Hono();
-app.use("*", cors());
-
-app.get("/health", (c) => c.json({ ok: true }));
-
+const app = createApp()
 const port = env.TRANSCRIBE_SUMMARIZE_SERVICE_PORT;
 
 await startMQ();
