961ea6704c6bc92df9a506d6d32054facb00bb2b
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-06-28 13:51:08 +0300

    split "get job" route into two

--- backend/services/api/src/controllers/jobs.controller.ts
+++ backend/services/api/src/controllers/jobs.controller.ts
@@ -1,13 +1,16 @@
 import type { Context } from "hono";
-import { and, eq } from "drizzle-orm";
+import { and, eq, ilike, lt, or, sql, type SQL } from "drizzle-orm";
 import {
   AudioTranscriptionJobs,
   db,
   TextSummarizationJobs,
 } from "../../../../shared/db";
+import { readTextFile } from "../../../../shared/bucket";
+import type { UploadId } from "../../../../shared/types/mq.types";
 import { CTX_KEYS } from "../../../../shared/keys";
+import type { JobSummaries } from "../schema/jobs.schema";
 
-export async function handleGetJob(c: Context) {
+export async function handleGetSummarizeJob(c: Context) {
   const userId = c.get(CTX_KEYS.userId);
   const uploadId = c.get(CTX_KEYS.uploadId);
 
@@ -32,6 +35,13 @@ export async function handleGetJob(c: Context) {
       error: textJob.error,
     });
 
+  return c.json({ message: "Job not found" }, 404);
+}
+
+export async function handleGetTranscribeJob(c: Context) {
+  const userId = c.get(CTX_KEYS.userId);
+  const uploadId = c.get(CTX_KEYS.uploadId);
+
   const [audioJob] = await db
     .select()
     .from(AudioTranscriptionJobs)
@@ -43,15 +53,45 @@ export async function handleGetJob(c: Context) {
     )
     .limit(1);
 
+  const [textJob] = await db
+    .select()
+    .from(TextSummarizationJobs)
+    .where(
+      and(
+        eq(TextSummarizationJobs.userId, userId),
+        (TextSummarizationJobs.audioUploadId, uploadId),
+      ),
+    );
   if (audioJob)
     return c.json({
       kind: "audio" as const,
       uploadId: audioJob.uploadId,
       fileName: audioJob.fileName,
       status: audioJob.status,
-
+      summary: textJob ? textJob.summary : null,
       error: audioJob.error,
     });
 
   return c.json({ message: "Job not found" }, 404);
+}
+
+export async function getUserJobs(c: Context) {
+  const userId = c.get(CTX_KEYS.userId);
+
+  const audio = await db
+    .select()
+    .from(AudioTranscriptionJobs)
+    .where(eq(AudioTranscriptionJobs.userId, userId));
+
+  const text = await db
+    .select()
+    .from(TextSummarizationJobs)
+    .where(eq(TextSummarizationJobs.userId, userId));
+
+  const jobs: JobSummaries = { audio, text };
+
+  return c.json({
+    jobs,
+    nextCursor,
+  });
 }
