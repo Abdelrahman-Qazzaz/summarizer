a8464f810cbc734608a37c7eaa5ef60d10c26a72
Abdelrahman Qazzaz <arandombird671@gmail.com>
2026-06-29 20:48:54 +0300

    routers to rerun jobs

--- backend/services/api/src/routes/jobs.router.ts
+++ backend/services/api/src/routes/jobs.router.ts
@@ -5,9 +5,14 @@ import { requireAuth } from "../middleware/auth.middleware";
 import {
   validateReqParams,
   validateReqQuery,
+  validateReqBody,
 } from "../middleware/validate.middleware";
 import { CTX_KEYS } from "../../../../shared/keys";
-import { jobReqParamSchema, jobsListQuerySchema } from "../schema/jobs.schema";
+import {
+  jobReqParamSchema,
+  jobsListQuerySchema,
+  jobRerunBodySchema,
+} from "../schema/jobs.schema";
 
 export const jobsRouter = new Hono();
 
@@ -34,6 +39,19 @@ jobsRouter.delete(
   `/summarize/:${CTX_KEYS.uploadId}`,
   validateReqParams(jobReqParamSchema),
   jobsController.handleDeleteSummarizeJob,
+);
+
+jobsRouter.post(
+  `/transcribe/:${CTX_KEYS.uploadId}/rerun`,
+  validateReqParams(jobReqParamSchema),
+  validateReqBody(jobRerunBodySchema),
+  jobsController.handleRerunTranscribeJob,
+);
+jobsRouter.post(
+  `/summarize/:${CTX_KEYS.uploadId}/rerun`,
+  validateReqParams(jobReqParamSchema),
+  validateReqBody(jobRerunBodySchema),
+  jobsController.handleRerunSummarizeJob,
 );
 
 jobsRouter.get(
